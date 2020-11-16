7c54d36d10bbd619e69c37255d201e6d7c8297c0
maciekekseke96 <maciek20145c@wp.pl>
2020-11-16 12:11:51 +0100

    Table structure prepared

--- src/Components/App/MainContent/TableContent/TableContent.js
+++ src/Components/App/MainContent/TableContent/TableContent.js
@@ -6,15 +6,81 @@ const TableContent = ({ stateDataFromAPI }) => {
   return (
     stateDataFromAPI && (
       <table className="charactersTable">
-        <thead></thead>
-        <tbody></tbody>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Culture</th>
+            <th>Books</th>
+            <th>Series Ammount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stateDataFromAPI.map((character, index) => {
+            const aliases = character.aliases;
+            const name = character.name;
+            let nameToDisplay;
+
+            if (name.length > 0 && aliases.length > 0) {
+              nameToDisplay = name + "," + aliases.join(",");
+            } else if (name.length < 1 && aliases.length > 0) {
+              nameToDisplay = aliases.join(",");
+            } else if (name.length > 0 && aliases.length < 0) {
+              nameToDisplay = name;
+            }
+
+            const books = character.books;
+            let booksToDisplay = [];
+
+            books.forEach((book) => {
+              const urlLength = book.length;
+
+              if (book[urlLength - 2] === "/") {
+                booksToDisplay.push(book[urlLength - 1]);
+              } else if (book[urlLength - 3] === "/") {
+                booksToDisplay.push(
+                  book.slice(book[urlLength - 2], book[urlLength - 1])
+                );
+              }
+            });
+
+            let seriesAmmount;
+
+            if (character.tvSeries[0].length < 1) {
+              seriesAmmount = 0;
+            } else {
+              seriesAmmount = character.tvSeries.length;
+            }
+
+            return (
+              <tr key={index}>
+                <td>{nameToDisplay}</td>
+                <td>
+                  {character.gender.length > 0 ? character.gender : "Unknown"}
+                </td>
+                <td>
+                  {" "}
+                  {character.culture.length > 0 ? character.culture : "Unknown"}
+                </td>
+                <td>
+                  <ul>
+                    {booksToDisplay.map((book, index) => {
+                      return <li key={index}>ID: {book}</li>;
+                    })}
+                  </ul>
+                </td>
+                <td>{seriesAmmount}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     )
   );
 };
 
 const mapStateToProps = (state) => ({
-  stateDataFromAPI: state.APIData,
+  stateDataFromAPI: state.APIData.data,
 });
 
 export default connect(mapStateToProps, {})(TableContent);
